4af8733ace4309b7231592875f7c783e203e4241
Svetlanaura <147398624+Svetlanaura@users.noreply.github.com>
2025-10-28 17:41:34 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Denisova Svetlana");
-        }
+            Scanner scanner = new Scanner(System.in);
+    System.out.println("Привет, покупатель!");
+    System.out.print("Введите цену товара (в руб.):");
+            int price = scanner.nextInt();
+    System.out.print("Введите вес товара (в кг.):");
+            int weight = scanner.nextInt();
+            int tax = Import.calculatedTax(price, weight);
+        System.out.println("Размер пошлины:" +tax+ "(в руб.)");
     }
+}
