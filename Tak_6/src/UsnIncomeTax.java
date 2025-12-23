eb2bb7ded44602e71c547340c8bde1ce89c281d3
Svetlanaura <147398624+Svetlanaura@users.noreply.github.com>
2025-12-23 16:22:13 +0300

    Add files via upload

---
+++ Tak_6/src/UsnIncomeTax.java
@@ -0,0 +1,10 @@
+package taxes;
+
+public class UsnIncomeTax extends TaxSystem {
+
+    @Override
+    public int calcTaxFor(int debit, int credit) {
+        return (int) (debit * 0.06);
+    }
+}
+
