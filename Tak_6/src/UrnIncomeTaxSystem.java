43eea052b3512fe9a62a185971565db715e5fcfa
Svetlanaura <147398624+Svetlanaura@users.noreply.github.com>
2025-12-23 16:20:23 +0300

    Delete Tak_6 directory

--- Tak_6/src/UrnIncomeTaxSystem.java
+++
@@ -1,9 +0,0 @@
-package taxes;
-
-public class UrnIncomeTaxSystem extends TaxSystem {
-    @Override
-    public int calcTaxFor(int debit, int credit) {
-        // Налог 6% от доходов
-        return Math.max(0, (int) Math.round(debit * 0.06));
-    }
-}
